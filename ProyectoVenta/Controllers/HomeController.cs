d1d928facb2a42e0d921b768cb8f59af2ae4c3f2
Maria Jose Jimenez Arce <mjimeneza@edu.upolitecnica.cr>
2024-07-30 18:20:39 -0600

    Se protegen los controladores.

---
+++ ProyectoVenta/Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoVenta.Datos;
+using ProyectoVenta.Models;
+using System.Xml.Linq;
+
+
+
+
+
+namespace ProyectoVenta.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        DA_Producto oProducto = new DA_Producto();
+        DA_Venta _daVenta = new DA_Venta();
+        DA_Producto _daproducto = new DA_Producto();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DetalleVenta()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult AutoCompleteProducto(string search)
+        {
+            List<Autocomplete> autocomplete = oProducto.Listar()
+                .Where(x => string.Concat(x.Codigo.ToUpper(), x.oCategoria.Descripcion.ToUpper(), x.Descripcion.ToUpper()).Contains(search.ToUpper()))
+                .Select(m => new Autocomplete
+                {
+                    label = $"{m.Codigo} - {m.oCategoria.Descripcion} - {m.Descripcion}",
+                    value = m.IdProducto
+                }).ToList();
+
+            return Json(autocomplete);
+        }
+
+        [HttpGet]
+        public JsonResult ObtenerProducto(int idproducto)
+        {
+            Producto _daProducto = _daproducto.Listar().FirstOrDefault(x => x.IdProducto == idproducto);
+            return Json(_daProducto);
+        }
+
+        [HttpPost]
+        public JsonResult RegistrarVenta([FromBody] Venta body)
+        {
+            string rpta = "";
+            {
+                XElement venta = new XElement("Venta",
+                    new XElement("TipoPago", body.TipoPago),
+                    new XElement("NumeroDocumento", "0"),
+                    new XElement("DocumentoCliente", body.DocumentoCliente),
+                    new XElement("NombreCliente", body.NombreCliente),
+                    new XElement("MontoPagoCon", body.MontoPagoCon),
+                    new XElement("MontoCambio", body.MontoCambio),
+                    new XElement("MontoSubTotal", body.MontoSubTotal),
+                    new XElement("MontoIGV", body.MontoIGV),
+                    new XElement("MontoTotal", body.MontoTotal)
+                );
+                XElement oDetalleVenta = new XElement("Detalle_Venta");
+                foreach (Detalle_Venta item in body.oDetalleVenta)
+                {
+                    oDetalleVenta.Add(new XElement("Item",
+                        new XElement("IdProducto", item.oProducto),
+                        //new XElement("IdProducto", item.IdProducto),
+                        new XElement("PrecioVenta", item.PrecioVenta),
+                        new XElement("Cantidad", item.Cantidad),
+                        new XElement("Total", item.Total)
+                    ));
+                }
+
+                venta.Add(oDetalleVenta);
+                rpta = _daVenta.Registrar(venta.ToString());
+
+                return Json(new { respuesta = rpta });
+            }
+        }
+
+        [HttpGet]
+       public JsonResult ObtenerVenta(string nrodocumento)// estaba como publico y lo pasamos a privado
+        {
+            Venta oVenta = _daVenta.Detalle(nrodocumento);
+            return Json(oVenta);
+        }
+    }
+}
---
+++ ProyectoVenta/Controllers/InventarioController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoVenta.Datos;
+using ProyectoVenta.Models;
+using System.Collections.Generic;
+
+namespace ProyectoVenta.Controllers
+{
+    [Authorize]
+    public class InventarioController : Controller
+    {
+        DA_Producto _daProducto = new DA_Producto();
+        DA_Categoria _daCategoria = new DA_Categoria();
+
+        public IActionResult Productos()
+        {
+            return View();
+        }
+
+        public IActionResult Categorias()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListaProducto()
+        {
+            List<Producto> oLista = _daProducto.Listar();
+            return Json(new { data = oLista });
+        }
+
+        [HttpPost]
+        public JsonResult GuardarProducto([FromBody] Producto obj)
+        {
+            string operacion = Request.Headers["operacion"];
+            bool respuesta;
+
+            if (operacion == "crear")
+            {
+                respuesta = _daProducto.Guardar(obj);
+            }
+            else
+            {
+                respuesta = _daProducto.Editar(obj);
+            }
+
+            return Json(new { respuesta = respuesta });
+        }
+
+        [HttpDelete]
+        public JsonResult EliminarProducto(int idproducto)
+        {
+            bool respuesta = _daProducto.Eliminar(idproducto);
+            return Json(new { respuesta = respuesta });
+        }
+
+        [HttpGet]
+        public JsonResult ListaCategoria()
+        {
+            List<Categoria> oLista = _daCategoria.Listar();
+            return Json(new { data = oLista });
+        }
+
+        [HttpPost]
+        public JsonResult GuardarCategoria([FromBody] Categoria obj)
+        {
+            string operacion = Request.Headers["operacion"];
+            bool respuesta;
+
+            if (operacion == "crear")
+            {
+                respuesta = _daCategoria.Guardar(obj);
+            }
+            else
+            {
+                respuesta = _daCategoria.Editar(obj);
+            }
+
+            return Json(new { respuesta = respuesta });
+        }
+
+        [HttpDelete]
+        public JsonResult EliminarCategoria(int idcategoria)
+        {
+            bool respuesta = _daCategoria.Eliminar(idcategoria);
+            return Json(new { respuesta = respuesta });
+        }
+    }
+}
+
