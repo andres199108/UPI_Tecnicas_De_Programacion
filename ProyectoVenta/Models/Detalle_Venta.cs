4c4c0cde2ad0769e64208606bbf5b4b52381333f
Andres Salazar V <andres.sv1991.app@gmail.com>
2024-07-30 18:57:32 -0600

    se protegieron los Models

---
+++ ProyectoVenta/Models/Detalle_Venta.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyectoVenta.Models
+{
+    public class Detalle_Venta
+    {
+       
+        public int IdDetalleVenta { get; set; }
+        public Producto? oProducto { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Total { get; set; }
+    }
+}
